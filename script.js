de1667c750085ec53123b8fde07592be6aa84d31
David Garbanzo <davidgq10@hotmail.com>
2022-08-19 00:04:30 -0600

    finished the pig game

--- script.js
+++ script.js
@@ -4,46 +4,104 @@
 //   Numbers.push(randomDiceRoll());
 // }
 // console.log(Numbers);
-let sumDice = 0;
+let totalScore = 0;
 let dice;
 let scorePlayer0 = 0;
 let scorePlayer1 = 0;
-let activePlayer = 'player--0';
+let activePlayer = 0;
 
 //Roll the dice
 document.querySelector('.btn--roll').addEventListener('click', () => {
+  //calc the dice
   dice = Math.floor(Math.random() * 6 + 1);
+  //show the dice img
   document.querySelector('.dice').src = 'dice-' + dice + '.png';
 
-  if (activePlayer === 'player--0') {
-    //scorePlayer0 = scorePlayer0 + dice;
-    sumDice = sumDice + dice;
-    console.log(`Nuevo puntaje del player--0 ${sumDice}`);
-    document.getElementById('score--1').textContent = sumDice;
-  }
-
-  console.log(dice, !(dice === 1));
-  if (!(dice === 1)) {
-    scorePlayer1 = scorePlayer1 + dice;
-    console.log(scorePlayer1);
+  if (dice === 1) {
+    //Add the dice to totalScore
+    restartScore();
+    showtotalScore();
+    switchPlayer();
+  } else {
+    totalScore = totalScore + dice;
+    showtotalScore();
   }
 });
 
 //Hold the score
 document.querySelector('.btn--hold').addEventListener('click', () => {
-  if (activePlayer === 'player--0') {
+  if (activePlayer) {
+    scorePlayer1 = scorePlayer1 + totalScore;
+    if (scorePlayer1 > 10) {
+      showWinMessage();
+    } else {
+      showPlayerScore(scorePlayer1);
+    }
     //scorePlayer0 = scorePlayer0 + dice;
-    scorePlayer0 = scorePlayer0 + sumDice;
-    console.log(`Nuevo puntaje del player--0 ${sumDice}`);
-    document.getElementById('current--1').textContent = scorePlayer0;
-    restartScore();
+  } else {
+    scorePlayer0 = scorePlayer0 + totalScore;
+
+    if (scorePlayer0 > 10) {
+      showWinMessage();
+    } else {
+      showPlayerScore(scorePlayer0);
+    }
   }
+  restartScore();
+  switchPlayer();
 });
 
-function restartScore(params) {
-  sumDice = 0;
-  document.getElementById('score--0').textContent = sumDice;
-  document.getElementById('score--1').textContent = sumDice;
+//Restart the game
+document.querySelector('.btn--new').addEventListener('click', () => {
+  newGame();
+});
+
+//Show the player score
+function showPlayerScore(score) {
+  document.getElementById('current--' + activePlayer).textContent = score;
 }
 
-function showSumDice(params) {}
+//Show win message
+function showWinMessage() {
+  document
+    .querySelector('.player--' + activePlayer)
+    .classList.add('player--winner');
+  document.getElementById('current--' + activePlayer).textContent = 'You win!';
+}
+
+//Restart the totalScore values
+function restartScore() {
+  totalScore = 0;
+  document.querySelectorAll('.score').forEach(i => {
+    i.textContent = 0;
+  });
+}
+
+//Restart all the game
+function newGame() {
+  scorePlayer0 = 0;
+  scorePlayer1 = 0;
+  activePlayer = 0;
+  document.querySelectorAll('.current-score').forEach(i => {
+    i.textContent = 0;
+  });
+  restartScore();
+  document.querySelectorAll('.player').forEach(i => {
+    i.classList.remove('player--winner');
+  });
+} //Show the totalScore on the player score
+function showtotalScore() {
+  document.getElementById('score--' + activePlayer).textContent = totalScore;
+}
+//Switch the current player
+function switchPlayer() {
+  document
+    .querySelector('.player--' + activePlayer)
+    .classList.remove('player--active');
+
+  activePlayer = activePlayer === 1 ? 0 : 1;
+
+  document
+    .querySelector('.player--' + activePlayer)
+    .classList.add('player--active');
+}
